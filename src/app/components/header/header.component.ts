2d67181987cc354a38af03e36c27c0512b5fb396
Hrushikesh Sampat Jadhav <hrushikesh.jadhav@accolite.com>
2023-02-27 17:39:43 +0530

    done

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  title = 'Task Tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {}
+
+   ngOnDestroy() {
+     // Unsubscribe to ensure no memory leaks
+     this.subscription.unsubscribe();
+   }
+
+  toggleAddTask() {
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return this.router.url === route;
+  }
+}
